561f31d0a2832a4a485c37f3bb4a0e2bc917927c
LinkInWorld <110659354+LinkInWorld@users.noreply.github.com>
2023-10-04 11:18:45 +0500

    Update neiroset.py

--- neiroset.py
+++ neiroset.py
@@ -1,37 +1,34 @@
 import streamlit as st
-
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing import image
 import numpy as np
 
 # Загрузите предварительно обученную модель h5
-model_path = 'путь_к_файлу/model.h5'
+model_path = 'https://disk.yandex.lt/d/6C6Dk6tiIMZAbw'
 model = keras.models.load_model(model_path)
 
-# Загрузите изображение, которое вы хотите классифицировать
-img = st.file_uploader("Pick a file")
-#img = 'путь_к_изображению/птица.jpg'
-#img = image.load_img(image_path, target_size=(224, 224))  # Предположим, что модель требует изображения размером 224x224 пикселя
-img = image.img_to_array(img)
-img = np.expand_dims(img, axis=0)
-
-# Выполните классификацию изображения
-predictions = model.predict(img)
-
-# Предположим, что модель возвращает вероятности классов
-# Найдите индекс класса с максимальной вероятностью
-predicted_class = np.argmax(predictions)
-
-# Загрузите файл с метками классов, чтобы узнать, какой класс соответствует предсказанию
+# Загрузите файл с метками классов
 class_labels_path = 'путь_к_файлу/class_labels.txt'
 with open(class_labels_path, 'r') as file:
     class_labels = [line.strip() for line in file.readlines()]
 
-# Определите название предсказанного класса
-predicted_label = class_labels[predicted_class]
+# Создайте Streamlit веб-приложение
+st.title('Определение вида птицы')
 
-# Выведите результат
+# Добавьте загрузку изображения через браузер
+uploaded_image = st.file_uploader("Загрузите изображение птицы", type=["jpg", "png", "jpeg"])
 
-st.write('Предсказанный класс:', predicted_label)
-#print(f'Предсказанный класс: {predicted_label}')
+if uploaded_image is not None:
+    img = image.load_img(uploaded_image, target_size=(224, 224))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+
+    # Выполните классификацию изображения
+    predictions = model.predict(img)
+    predicted_class = np.argmax(predictions)
+    predicted_label = class_labels[predicted_class]
+
+    # Выведите результат
+    st.image(uploaded_image, caption='Загруженное изображение', use_column_width=True)
+    st.write(f'Предсказанный класс: {predicted_label}')
