ceb81acf34b7bd677533b8ab4f789ede6d47de9a
LinkInWorld <110659354+LinkInWorld@users.noreply.github.com>
2023-10-04 11:39:36 +0500

    Update neiroset.py

--- neiroset.py
+++ neiroset.py
@@ -7,5 +7,5 @@ st.title('Определение вида птицы')
 uploaded_image = st.file_uploader("Загрузите изображение птицы", type=["jpg", "png", "jpeg"])
 
     # Выведите результат
-    st.image(uploaded_image, caption='Загруженное изображение', use_column_width=True)
-    st.write(f'Предсказанный класс: {predicted_label}')
+st.image(uploaded_image, caption='Загруженное изображение', use_column_width=True)
+st.write(f'Предсказанный класс: {predicted_label}')
