10cfac4e31abffbe481ac30f99506672d1802680
LinkInWorld <110659354+LinkInWorld@users.noreply.github.com>
2023-10-10 15:45:48 +0500

    Update neiroset.py

--- neiroset.py
+++ neiroset.py
@@ -9,6 +9,7 @@ from tensorflow.keras.preprocessing import image
 
 # model= keras.models.load_model('./EfficientNetB0-525-(224 X 224)- 98.97.h5', custom_objects={'F1_score':'F1_score'})
 model_path = '/EfficientNetB0-525-(224 X 224)- 98.97.h5'
+model= keras.models.load_model(model_path, custom_objects={'F1_score':'F1_score'})
 
 # Создайте Streamlit веб-приложение
 st.title('Определение вида птицы')
