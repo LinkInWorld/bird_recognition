83e5adb025b088dee1f8a7864f82c1524b612de8
LinkInWorld <110659354+LinkInWorld@users.noreply.github.com>
2023-10-10 14:22:39 +0500

    Update neiroset.py

--- neiroset.py
+++ neiroset.py
@@ -9,6 +9,7 @@ from tensorflow.keras.preprocessing import image
 
 # Загрузите предварительно обученную модель h5 https://github.com/LinkInWorld/bird_recognition/releases
 wget -O model.h5 "https://drive.google.com/uc?export=download&id=1Iur8carY3Izn7CxPdYGHOOA5DiN0Gj2v"
+model= keras.models.load_model('model.h5', custom_objects={'F1_score':'F1_score'})
 
 # Создайте Streamlit веб-приложение
 st.title('Определение вида птицы')
