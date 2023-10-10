a33def2bfcaee116e37f075c14abf998bdead7ee
LinkInWorld <110659354+LinkInWorld@users.noreply.github.com>
2023-10-10 15:14:07 +0500

    Update neiroset.py

--- neiroset.py
+++ neiroset.py
@@ -10,7 +10,7 @@ from tensorflow.keras.preprocessing import image
 # Загрузите предварительно обученную модель h5 https://github.com/LinkInWorld/bird_recognition/releases
 # !wget -O model.h5 "https://drive.google.com/uc?export=download&id=1Iur8carY3Izn7CxPdYGHOOA5DiN0Gj2v"
 # model= keras.models.load_model('model.h5', custom_objects={'F1_score':'F1_score'})
-model = keras.models.load_model('./releases/model.h5', custom_objects={'F1_score':'F1_score'})
+# model = keras.models.load_model('./releases/model.h5', custom_objects={'F1_score':'F1_score'})
 # class_labels =
 
 
@@ -20,10 +20,6 @@ st.title('Определение вида птицы')
 # Добавьте загрузку изображения через браузер
 uploaded_image = st.file_uploader("Загрузите изображение птицы", type=["jpg", "png", "jpeg"])
 
-
-if uploaded_image is not None:
-  st.image(uploaded_image, caption='Загруженное изображение', use_column_width=True)
-  st.write(f'Предсказанный класс: Гусь')
 
 if uploaded_image is not None:
     img = image.load_img(uploaded_image, target_size=(224, 224), grayscale=False)
